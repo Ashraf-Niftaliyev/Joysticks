bec5bb4b59849d74688c0655b1ba9cc11f9e4a68
Ashraf Niftaliyev <nifteliyevesref@gmail.com>
2023-05-31 16:15:31 +0400

    Welcome screen created

---
+++ app/src/main/java/com/example/joysticks/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.joysticks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.joysticks.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
